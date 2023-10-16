0b07923160ced43d38ea75ae4e6b69e260e6822b
Wilber Jose Matus Gonzalez <wilberj1987@gmail.com>
2023-10-16 16:01:54 -0600

    profiles-ext/i-sadav#1766 revert submenu

--- SNI_UI2/wwwroot/App.js
+++ SNI_UI2/wwwroot/App.js
@@ -1,83 +1,12 @@
 import { css } from "./WDevCore/WModules/WStyledRender.js";
 
+const data =   [
+    { name: "imagen1", imagen: "./Media/Image/avatar.png"},
+    { name: "imagen1", imagen: "./Media/Image/avatar.png"},
+    { name: "imagen1", imagen: "./Media/Image/avatar.png"},
+]
 const OnLoad = async () => {    
-    Main.append("hola mundo");
+    //Main.append("hola mundo");
+    Main.innerHTML = data.map(x => `<img src="${x.imagen}">`).join("");
 }
-const cssCus = css`
-    .dep-container{
-        display: grid;
-        grid-template-columns: 30% 30% 30%;
-        gap: 20px;
-        padding: 20px;
-    }
-    .card {
-        padding: 20px;
-        background-color: #0e8bd9;
-        display: grid;
-        grid-template-rows: 40px 20px 70px;
-        color: #fff;
-        border-radius: 15px;
-    }
-    .card h1 {
-        font-size: 24px;
-        margin: 5px 0px;
-    }
-    .cont-mini-cards {
-        width: 100%;
-        display: flex;
-        gap: 5px;
-    }
-    .mini-card {
-        padding: 8px;
-        background-color: #fff;
-        font-size: 11px;
-        border-radius: 10px;
-        color: #444;
-        margin: 10px 0px;
-        display: flex;
-        flex-wrap: wrap;
-        height: 21px;
-        align-items: center;
-        gap:5px;
-    }
-    .mini-card img{ 
-        height: 20px;
-        width: 20px;
-        object-fit: contain;
-    }
-    @media (max-width: 1300px){
-        .dep-container{
-            display: grid;
-            grid-template-columns: 50% 50%;
-            gap: 20px;
-            padding: 20px;            
-        }
-        .cont-mini-cards {
-            flex-direction: column;
-        }
-        .mini-card {
-            padding: 8px;
-            color: #444;
-            margin: 10px 0px 0px 0px;
-            height: 21px;
-        }
-        .card {
-            padding: 20px;
-            background-color: #0e8bd9;
-            display: grid;
-            grid-template-rows: 40px 20px auto;
-            color: #fff;
-            border-radius: 15px;
-        }
-    }
-    @media (max-width: 800px){
-        .dep-container{
-            display: grid;
-            grid-template-columns: 100%;
-            gap: 20px;
-            padding: 20px;
-        }
-
-    }
-`
 window.onload = OnLoad;
